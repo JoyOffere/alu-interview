fa7394426da86f7826af3d564d61b72251612647
Joy Offere <113602216+JoyOffere@users.noreply.github.com>
2024-02-23 17:56:41 +0300

    Pascal pascal_triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,64 @@
 #!/usr/bin/python3
+# 12-pascal_triangle.py
+"""A python script that
+passes the pascal triangle checks
 """
-0-main
-"""
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-def print_triangle(triangle):
+
+def pascal_triangle(n):
+    """The function doing
+    pascal triangle it was beautiful
+    and technical to write
     """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+    list_of_list = []
+    if n <= 0:
+        return list_of_list
+    else:
+        if n == 1:
+            list1 = [1]
+            list_of_list.append(list1)
+            return list_of_list
+        else:
+            for i in range(n):
+                if i == 0:
+                    list2 = [1]
+                    list_of_list.append(list2)
+                else:
+                    list3 = list_of_list[i - 1]
+                    list4 = []
+                    for j in range(i + 1):
+                        if j == 0:
+                            list4.append(list3[j] + 0)
+                        elif j > 0 and len(list3) != j:
+                            list4.append(list3[j - 1] + list3[j])
+                        else:
+                            list4.append(list3[j - 1] + 0)
+                    list_of_list.append(list4)
+    return list_of_list
 
 
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+
+
+
+
+
+
+
+
+
+# #!/usr/bin/python3
+# """
+# 0-main
+# """
+# pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+# def print_triangle(triangle):
+#     """
+#     Print the triangle
+#     """
+#     for row in triangle:
+#         print("[{}]".format(",".join([str(x) for x in row])))
+
+
+# if __name__ == "__main__":
+#     print_triangle(pascal_triangle(5))
